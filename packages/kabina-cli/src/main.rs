ad644bdb2e719df2222babbe4580f181fb47695d
Stanislav Panferov <stanislav@panferov.dev>
2023-03-04 12:19:41 -0800

    `drive!` macro for code re-use

--- packages/kabina-cli/src/main.rs
+++ packages/kabina-cli/src/main.rs
@@ -1,8 +1,11 @@
+#![feature(decl_macro)]
+
 use std::{error::Error, path::PathBuf, sync::Arc};
 
 use clap::Parser;
 use kabina_db::{
-    bundle_files, runtime::Runtime, ApplyTransform, Cause, ResolveRootFiles, RuntimeTask,
+    bundle_files, runtime::Runtime, ApplyTransform, Cause, Executable, ResolveRootFiles,
+    RuntimeTask, SharedDatabase,
 };
 
 use kabina_rt::DenoRuntime;
@@ -54,33 +57,40 @@ fn main() -> Result<(), Box<dyn Error>> {
                 bundle
             };
 
-            loop {
-                let mut tasks = Vec::new();
-                match bundle_files(&*db.read(), schema, bundle) {
-                    Ok(files) => {
-                        println!("Bundle files {:?}", files);
-                        break;
-                    }
-                    Err(Cause::Pending) => {
-                        tasks = kabina_db::bundle_files::accumulated::<RuntimeTask>(
-                            &*db.read(),
-                            schema,
-                            bundle,
-                        );
-                    }
-                    Err(e) => panic!("{:?}", e),
-                }
-
-                for task in tasks {
-                    if let Some(task) = task.downcast_ref::<ResolveRootFiles>() {
-                        task.resolve(&mut db.write())
-                    } else if let Some(task) = task.downcast_ref::<ApplyTransform>() {
-                        tokio.block_on(runtime.transform(task));
-                    }
-                }
-            }
+            tokio.block_on(drive!(runtime, bundle_files(db, schema, bundle)));
         }
     }
 
     Ok(())
+}
+
+pub macro drive($rt:expr, $func:ident($db:expr, $($arg:expr),+)) {
+    async { loop {
+        #[allow(unused_assignments)]
+        let mut tasks = Vec::new();
+        match $func(&*$db.read(), $($arg),+) {
+            Ok(result) => {
+                break result;
+            }
+            Err(Cause::Pending) => {
+                tasks = $func::accumulated::<RuntimeTask>(
+                    &*$db.read(), $($arg),+
+                );
+            }
+            Err(e) => panic!("{:?}", e),
+        }
+
+        for task in tasks {
+            // TODO: parallel
+            drive_task(&*task, &$db, &mut $rt).await
+        }
+    } }
+}
+
+pub async fn drive_task(task: &dyn Executable, db: &SharedDatabase, rt: &mut Box<impl Runtime>) {
+    if let Some(task) = task.downcast_ref::<ResolveRootFiles>() {
+        task.resolve(&mut db.write())
+    } else if let Some(task) = task.downcast_ref::<ApplyTransform>() {
+        rt.transform(task).await;
+    }
 }
